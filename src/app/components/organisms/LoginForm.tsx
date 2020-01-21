7b0bbd5a3763021f9485041c1d36fd41740554a0
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2020-01-21 18:52:46 +0900

    ログイン画面のデザイン

--- src/app/components/organisms/LoginForm.tsx
+++ src/app/components/organisms/LoginForm.tsx
@@ -38,29 +38,80 @@ const LoginForm: NextPage<Props> = (props) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
+      <div className='data-area'>
         <label htmlFor='email'>Email</label>
         <input
           type='email'
           name='email'
           id='email'
+          className='input-textbox'
           value={email}
           onChange={updateEmail}
         />
       </div>
-      <div>
+      <div className='data-area'>
         <label htmlFor='password'>Password</label>
         <input
           type='password'
           name='password'
           id='password'
+          className='input-textbox'
           value={password}
           onChange={updatePassword}
         />
       </div>
       <div>
-        <input type='submit' value='ログイン' />
+        <input className='input-button' type='submit' value='ログイン' />
       </div>
+      <style jsx>{`
+        form {
+          padding: 20vh 1rem 0;
+        }
+        .data-area {
+          padding-top: 1rem;
+        }
+        .data-area > label {
+          display: inline-block;
+          font-size: 1.4rem;
+          width: 9rem;
+        }
+        .input-textbox {
+          box-sizing: border-box;
+          background-color: unset;
+          border: solid 2px #00BCD4;
+          border-radius: 1.2rem;
+          color: #00BCD4;
+          font-size: 1.2rem;
+          padding: 0.5rem;
+          width: 100%;
+        }
+        .input-button {
+          box-sizing: border-box;
+          background-color: unset;
+          border: solid 2px #00BCD4;
+          border-radius: 1.375rem;
+          color: #00BCD4;
+          font-size: 1.2rem;
+          padding: 0.5rem 1rem;
+          transition: 0.2s all;
+          margin: 1rem 0 0 9rem;
+        }
+
+        .input-button:hover {
+          background-color: #00BCD4;
+          color: white;
+        }
+
+        input:focus {
+          outline: none;
+        }
+
+        @media (min-width: 768px) {
+          .input-textbox {
+            width: 23rem;
+          }
+        }
+      `}</style>
     </form>
   );
 };
