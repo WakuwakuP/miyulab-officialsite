ce290db1241bad08c3194a6f4cce5e1d623894ca
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2020-01-21 14:47:29 +0900

    dotenv

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack');
 module.exports = {
   distDir: '../../dist/functions/next',
+  webpack: config => {
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
+
+    return config
+  }
 }
