f0ab44f9932079b910135d5a185e99067e5b5ef3
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2022-09-05 20:40:19 +0900

    Update

--- src/app/src/pages/_app.tsx
+++ src/app/src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const url = process.env.BASE_URL
+  return (
+    <>
+      <Head>
+        <title>Miyulab</title>
+        <link rel="canonical" href={url} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
