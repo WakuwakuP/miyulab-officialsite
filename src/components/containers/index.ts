d8df66af748b8b9b844305de63af8edacc9dd0da
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2022-11-06 18:26:54 +0900

    ビルドエラーの修正

---
+++ src/components/containers/index.ts
@@ -0,0 +1,4 @@
+export { default as ContentCard } from './ContentCard'
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as Pagination } from './Pagination'
