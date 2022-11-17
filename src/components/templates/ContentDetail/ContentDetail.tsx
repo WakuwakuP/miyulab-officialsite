5d01ff3c802851a89346d50ed1dbf84fa31f7ea1
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2022-11-17 11:19:21 +0900

    add Toc #41 close

--- src/components/templates/ContentDetail/ContentDetail.tsx
+++ src/components/templates/ContentDetail/ContentDetail.tsx
@@ -3,13 +3,18 @@ import Link from 'next/link'
 import type { Category } from 'types/Category'
 import type { Content } from 'types/Content'
 
+import { Toc } from 'components/containers'
 import { PageTitle } from 'components/parts'
 import styles from 'styles/components/templates/ContentDetail.module.css'
 import 'highlight.js/styles/github-dark.css'
 
 export interface ContentDetailProps {
   content: Content
-  toc: unknown
+  toc: {
+    id: string
+    text: string
+    name: string
+  }[]
 }
 
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
@@ -25,12 +30,17 @@ export const ContentDetail = ({ content, toc }: ContentDetailProps) => {
           </Link>
         </li>
       ))}
-      <div
-        className={styles.content}
-        dangerouslySetInnerHTML={{
-          __html: `${content.content}`,
-        }}
-      />
+      <div className={styles.mainarea}>
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{
+            __html: `${content.content}`,
+          }}
+        />
+        <div className={styles.toc}>
+          <Toc toc={toc} />
+        </div>
+      </div>
     </>
   )
 }
