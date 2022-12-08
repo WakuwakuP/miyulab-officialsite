fc54228ad794c7d0441707e431307bfd84061aa1
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2022-12-08 18:05:10 +0900

    fix

--- src/components/parts/Particle/Particle.tsx
+++ src/components/parts/Particle/Particle.tsx
@@ -18,84 +18,85 @@ export const Particle = ({ id }: ParticleProps) => {
   }, [])
   return (
     <>
-      <Particles
-        id={id}
-        init={partioclesInit}
-        loaded={partioclesLoaded}
-        data-chromatic='ignore'
-        options={{
-          background: {
-            color: {
-              value: '#000000',
+      <div data-chromatic='ignore'>
+        <Particles
+          id={id}
+          init={partioclesInit}
+          loaded={partioclesLoaded}
+          options={{
+            background: {
+              color: {
+                value: '#000000',
+              },
+              opacity: 0.1,
             },
-            opacity: 0.1,
-          },
-          particles: {
-            color: {
-              value: ['#ff77ff', '#77ffff', '#ffff77'],
-            },
-            line_linked: {
-              color: '#ffffff',
-              distance: 200,
-              enable: false,
-              opacity: 1,
-              width: 2,
-            },
-            move: {
-              attract: {
+            particles: {
+              color: {
+                value: ['#ff77ff', '#77ffff', '#ffff77'],
+              },
+              line_linked: {
+                color: '#ffffff',
+                distance: 200,
                 enable: false,
-                rotateX: 600,
-                rotateY: 1200,
+                opacity: 1,
+                width: 2,
               },
-              bounce: false,
-              direction: 'none',
-              enable: true,
-              out_mode: 'out',
-              random: false,
-              speed: 3,
-              straight: false,
-            },
-            number: {
-              density: {
+              move: {
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
+                bounce: false,
+                direction: 'none',
                 enable: true,
-                value_area: 800,
+                out_mode: 'out',
+                random: false,
+                speed: 3,
+                straight: false,
               },
-              value: 30,
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 30,
+              },
+              opacity: {
+                anim: {
+                  enable: true,
+                  opacity_min: 0.3,
+                  speed: 0.1,
+                  sync: false,
+                },
+                random: true,
+                value: 0.3,
+              },
+              shape: {
+                polygon: {
+                  nb_sides: 6,
+                },
+                stroke: {
+                  color: '#000',
+                  width: 0,
+                },
+                type: `circle`,
+              },
+              size: {
+                animation: {
+                  enable: true,
+                  size_min: 70,
+                  speed: 10,
+                  sync: false,
+                },
+                random: true,
+                value: 150,
+              },
             },
-            opacity: {
-              anim: {
-                enable: true,
-                opacity_min: 0.3,
-                speed: 0.1,
-                sync: false,
-              },
-              random: true,
-              value: 0.3,
-            },
-            shape: {
-              polygon: {
-                nb_sides: 6,
-              },
-              stroke: {
-                color: '#000',
-                width: 0,
-              },
-              type: `circle`,
-            },
-            size: {
-              animation: {
-                enable: true,
-                size_min: 70,
-                speed: 10,
-                sync: false,
-              },
-              random: true,
-              value: 150,
-            },
-          },
-          retina_detect: true,
-        }}
-      />
+            retina_detect: true,
+          }}
+        />
+      </div>
     </>
   )
 }
