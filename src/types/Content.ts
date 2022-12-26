a7b439593f53a593df9017d3f9f1202970202453
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2022-12-26 10:48:33 +0900

    Add /feed

--- src/types/Content.ts
+++ src/types/Content.ts
@@ -1,11 +1,12 @@
 import type { Category } from './Category'
+import type { ContentBody } from './ContentBody'
 import type { ContentsCategory } from './ContentsCategory'
 import type { MicroCMSImage } from 'microcms-js-sdk'
 
 export interface Content {
   id: string
   title: string
-  content: string
+  content: ContentBody[]
   thumbnail?: MicroCMSImage
   category: Category[]
   contentsCategory: ContentsCategory[]
