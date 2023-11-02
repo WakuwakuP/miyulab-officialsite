c06bfdb62e2c6c6d52fb3c0a88e87e535ef4daba
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2023-11-02 10:28:02 +0900

    Upgrade Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,23 +5,25 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-next-router',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
+
   features: {
     storyStoreV7: true,
   },
+
   webpackFinal(config) {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')]
 
     config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()]
     return config
   },
+
+  docs: {
+    autodocs: true
+  }
 }
