34e93c2c6ed81f5adfcf078d1ea4fc74ea6d121a
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2023-12-03 17:30:14 +0900

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
           rel='stylesheet'
         ></link>
         {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_TOKEN && (
-          <Script
+          <script
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_TOKEN}`}
             crossOrigin='anonymous'
