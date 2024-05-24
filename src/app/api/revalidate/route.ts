bd8129938418fea7b43a35bfedd96b40bbf91877
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2024-05-24 15:43:04 +0900

    fix

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -9,16 +9,16 @@ export async function POST(request: Request) {
   if (data.id === undefined) return new Response(null, { status: 400 })
 
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  revalidatePath(`/content/detail/${data.id}`, 'page')
+  revalidatePath(`/content/detail/${data.id}`)
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   console.log(`revalidate: /content/detail/${data.id}`)
 
   if (data.contents.old?.status[0] !== data.contents.new?.status[0]) {
-    revalidatePath('/', 'page')
-    revalidatePath('/content/latest/', 'page')
-    revalidatePath('/content/latest/[page]', 'page')
-    revalidatePath('/content/category/[categoryId]/', 'page')
-    revalidatePath('/content/category/[categoryId]/[page]', 'page')
+    revalidatePath('/')
+    revalidatePath('/content/latest/')
+    revalidatePath('/content/latest/[page]')
+    revalidatePath('/content/category/[categoryId]/')
+    revalidatePath('/content/category/[categoryId]/[page]')
     revalidatePath('/feed')
 
     console.log('revalidate: /')
