07fdd58252d18626cfcf3275abb3b4f95473c961
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2024-05-29 19:29:05 +0900

    fix

--- src/app/content/category/[categoryId]/[page]/page.tsx
+++ src/app/content/category/[categoryId]/[page]/page.tsx
@@ -30,7 +30,7 @@ const getCacheContentsCategory = (categoryId: string, page: string) =>
     async () => {
       return await getContentsCategory(categoryId, page)
     },
-    ['contents-category'],
+    [`contents-category-${categoryId}-page-${page}`],
     {
       tags: ['contents-category'],
     },
